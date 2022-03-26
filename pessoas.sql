68212732b55b2e10bec7efccebf7109dab749d0a
Roberta Seneda Vilella <98057209+rseneda@users.noreply.github.com>
2022-03-26 16:54:53 -0400

    Create pessoas.sql

---
+++ pessoas.sql
@@ -0,0 +1,8 @@
+-- Um crime ocorreu e o detetive precisa de sua ajuda. --
+-- O detetive deu a você o relatório da cena do crime, mas de alguma forma você o perdeu. --
+-- Você lembra vagamente que o crime foi um homicídio ocorrido em 15 de janeiro de 2018 e que ocorreu em SQL City. --
+-- Comece recuperando o relatório da cena do crime correspondente do banco de dados do departamento de polícia. --
+
+-- alterar 'pessoa' -- limite 10
+SELECT count(*)
+FROM person;
