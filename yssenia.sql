1ea743ace2db4f57c11b2dc87769cd14003de884
Roberta Seneda Vilella <98057209+rseneda@users.noreply.github.com>
2022-03-26 17:07:11 -0400

    Create yssenia.sql

---
+++ yssenia.sql
@@ -0,0 +1,7 @@
+-- Um crime ocorreu e o detetive precisa de sua ajuda. --
+-- O detetive deu a você o relatório da cena do crime, mas de alguma forma você o perdeu. --
+-- Você lembra vagamente que o crime foi um homicídio ocorrido em 15 de janeiro de 2018 e que ocorreu em SQL City. --
+-- Comece recuperando o relatório da cena do crime correspondente do banco de dados do departamento de polícia. --
+
+-- consulta simples para obter tudo sobre uma pessoa específica --
+SELECT * FROM person WHERE name = 'Yessenia Fossen"
