31198ba0e82a80bef17c6b68ebbae4a83fb52d8d
Roberta Seneda Vilella <98057209+rseneda@users.noreply.github.com>
2022-03-26 16:53:27 -0400

    Create banco_dados.sql

---
+++ banco_dados.sql
@@ -0,0 +1,7 @@
+-- Um crime ocorreu e o detetive precisa de sua ajuda. --
+-- O detetive deu a você o relatório da cena do crime, mas de alguma forma você o perdeu. --
+-- Você lembra vagamente que o crime foi um homicídio ocorrido em 15 de janeiro de 2018 e que ocorreu em SQL City. --
+-- Comece recuperando o relatório da cena do crime correspondente do banco de dados do departamento de polícia. --
+
+-- quais valores estão no banco de dados --
+SELECT DISTINCT type FROM crime_scene_report;
