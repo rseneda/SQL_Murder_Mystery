614dfe2c5ba22da9dd339f6cc17431bfdd0397fa
Roberta Seneda Vilella <98057209+rseneda@users.noreply.github.com>
2022-03-26 16:56:42 -0400

    Create crime_cidade.sql

---
+++ crime_cidade.sql
@@ -0,0 +1,10 @@
+-- Um crime ocorreu e o detetive precisa de sua ajuda. --
+-- O detetive deu a você o relatório da cena do crime, mas de alguma forma você o perdeu. --
+-- Você lembra vagamente que o crime foi um homicídio ocorrido em 15 de janeiro de 2018 e que ocorreu em SQL City. --
+-- Comece recuperando o relatório da cena do crime correspondente do banco de dados do departamento de polícia. --
+
+-- relatórios sobre um tipo específico de crime em uma cidade específica --
+
+SELECT * FROM crime_scene_report 
+WHERE type = 'theft' 
+AND city = 'Chicago';
